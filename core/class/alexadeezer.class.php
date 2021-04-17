bb58959a53214a33140bc5a76a82a17e0fa4ab1f
Sigalou <sigalou@sigalou-domotique.fr>
2021-04-17 14:00:24 +0200

    correction warnings

--- core/class/alexadeezer.class.php
+++ core/class/alexadeezer.class.php
@@ -554,11 +554,17 @@ class alexadeezerCmd extends cmd {
 		return str_replace('#volume#', $_options['slider'], $request);
 	}
 	
-	public function getWidgetTemplateCode($_version = 'dashboard', $_noCustom = false) {
-		if ($_version != 'scenario') return parent::getWidgetTemplateCode($_version, $_noCustom);
+//	public function getWidgetTemplateCode($_version = 'dashboard', $_noCustom = false) {
+    public function getWidgetTemplateCode($_version = 'dashboard', $_clean = true, $_widgetName = '') {
+
+//		if ($_version != 'scenario') return parent::getWidgetTemplateCode($_version, $_noCustom);
+        if ($_version != 'scenario') return parent::getWidgetTemplateCode($_version, $_clean, $_widgetName);
+
 		list($command, $arguments) = explode('?', $this->getConfiguration('request'), 2);
 		if ($command == 'command' && strpos($arguments, '#select#')) 
 			return getTemplate('core', 'scenario', 'cmd.command', 'alexadeezer');
-		return parent::getWidgetTemplateCode($_version, $_noCustom);
+//		return parent::getWidgetTemplateCode($_version, $_noCustom);
+        return parent::getWidgetTemplateCode($_version, $_clean, $_widgetName);
+
 	}
 }
